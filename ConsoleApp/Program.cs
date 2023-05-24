98d1824bdba7338017926f74c989f1ba6c123bae
parkjin <104075813+staronthepark@users.noreply.github.com>
2023-05-24 11:20:08 +0900

    Add Projects

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MayBLib;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = TestClass.DLL_Sum(2d, 4d);
+
+            Console.WriteLine("The sum is " + sum.ToString());
+        }
+    }
+}
---
+++ MayBLib/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MayBLib
+{
+    public class TestClass
+    {
+        [DllImport("MayBCppLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        extern public static double DLL_Sum(double a, double b);
+    }
+}
